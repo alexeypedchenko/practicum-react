885b1442d88317fcbb47e6d8a4e310bccf048225
alex <alex.pedchenko@gmail.com>
2021-11-02 21:58:27 +0200

    fixed constructor dnd

--- src/components/burger-constructor/burger-constructor-item/BurgerConstructorItem.tsx
+++ src/components/burger-constructor/burger-constructor-item/BurgerConstructorItem.tsx
@@ -5,14 +5,14 @@ import { useDrag, useDrop, XYCoord } from 'react-dnd'
 // redux
 import { useDispatch } from 'react-redux'
 import { changeItemsPosition } from '../../../store/slices/burgerConstructorSlice'
-import { IBurgerConstructorItemProps } from '../../../types/types'
+import { IBurgerConstructorItemProps, IBurgerIngredient } from '../../../types/types'
 
 const BurgerConstructorItem: FC<IBurgerConstructorItemProps> = ({ item, icon, handleClose, isLocked, type, index }) => {
   const dispatch = useDispatch()
   const ref = useRef<HTMLDivElement> (null)
   const [, drop] = useDrop({
     accept: 'constructorItem',
-    hover(item: {index: number | undefined}, monitor) {
+    hover(item: IBurgerIngredient & {index: number}, monitor) {
       if (!ref.current) return
       const dragIndex = item.index
       const hoverIndex: number | undefined = index
@@ -20,7 +20,11 @@ const BurgerConstructorItem: FC<IBurgerConstructorItemProps> = ({ item, icon, ha
       const hoverBoundingRect: DOMRect = ref.current?.getBoundingClientRect()
       const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
       const clientOffset: XYCoord | null = monitor.getClientOffset()
-      if (!dragIndex || !hoverIndex || !clientOffset) return
+      if (
+        (dragIndex === undefined)
+        || (hoverIndex === undefined)
+        || (clientOffset === null)
+      ) return
       const hoverClientY = clientOffset.y - hoverBoundingRect.top
       if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) return
       if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) return
